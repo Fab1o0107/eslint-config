d1a738588795122953cefa50d169d4938f3eb36c
Fabio0107ID <fabio0107idcraft@gmail.com>
2022-02-26 22:03:40 +0700

    feat(rules): disable `sort-keys`, `class-methods-use-this`, and `prefer-named-capture-group` rules

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 /* eslint-env node */
 const { resolve } = require("path");
 
-module.exports.extends = resolve(require.resolve("@rahagia/eslint-config"));
+module.exports = {
+    extends: resolve(require.resolve("@rahagia/eslint-config")),
+    rules: {
+        "sort-keys": "off",
+        "class-methods-use-this": "off",
+        "prefer-named-capture-group": "off"
+    }
+};
