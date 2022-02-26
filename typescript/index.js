468b45e779327462f207e235e5d376de84f46b78
Fabio0107ID <fabio0107idcraft@gmail.com>
2022-02-26 22:04:45 +0700

    feat(typescript#rules): disable `no-unsafe-call`, `unbound-method`, and `no-dynamic-delete` rules

--- typescript/index.js
+++ typescript/index.js
@@ -6,6 +6,9 @@ module.exports = {
     extends: [resolve(__dirname, "..", "index.js"), resolve(require.resolve("@rahagia/eslint-config/typescript"))],
     rules: {
         "@typescript-eslint/no-var-requires": "off",
-        "@typescript-eslint/no-require-imports": "off"
+        "@typescript-eslint/no-require-imports": "off",
+        "@typescript-eslint/no-unsafe-call": "off",
+        "@typescript-eslint/unbound-method": "off",
+        "@typescript-eslint/no-dynamic-delete": "off"
     }
 };
