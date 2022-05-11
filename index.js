301469b0b06077085802d5b0150fc9f09cd84f0e
Fab1o0107 <fabio0107idcraft@gmail.com>
2022-05-11 22:57:03 +0700

    refactor(extends): use `@clytage-pkg/eslint-config` instead

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const { resolve } = require("path");
 
 module.exports = {
-    extends: resolve(require.resolve("@rahagia/eslint-config")),
+    extends: resolve(require.resolve("@clytage-pkg/eslint-config")),
     rules: {
         "sort-keys": "off",
         "class-methods-use-this": "off",
