c27fb0973b8c30821ac55600f6cfdf464fb2fdad
Fab1o0107 <fabio0107idcraft@gmail.com>
2022-05-11 22:57:03 +0700

    refactor(extends): use `@clytage-pkg/eslint-config` instead

--- typescript/index.js
+++ typescript/index.js
@@ -2,7 +2,7 @@
 const { resolve } = require("path");
 
 module.exports = {
-    extends: [resolve(__dirname, "..", "index.js"), resolve(require.resolve("@rahagia/eslint-config/typescript"))],
+    extends: [resolve(__dirname, "..", "index.js"), resolve(require.resolve("@clytage-pkg/eslint-config/typescript"))],
     rules: {
         "@typescript-eslint/no-var-requires": "off",
         "@typescript-eslint/no-require-imports": "off",
